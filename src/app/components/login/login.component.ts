498f7d1cbc06ee2d41501563318bfc9c0031bd7b
karthik-karotra <karthikpatel54@gmail.com>
2020-09-24 03:38:34 +0530

    [Karthik]REFACTOR : Added Error Messages For Invalid Email And Password

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,6 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   constructor() { }
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required, Validators.minLength(8)]);
+
+  getErrorMessageForEmail() {
+    return this.email.hasError('required') ? 'Email field cannot be blank' :
+      this.email.hasError('email') ? 'Not a valid email' :
+        '';
+  }
+  getErrorMessageForPassword() {
+    return this.password.hasError('required') ? 'Password cannot be blank' :
+      this.password.hasError('password') ? 'Not a valid password' :
+        '';
+  }
 
   ngOnInit(): void { }
 }
